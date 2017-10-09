40ad6df74bf870abe87c0c8c19e397008f4e6813
RobertOrtiz87 <ortizalvarez.r@gmail.com>
2017-10-09 16:24:47 -0300

    Department and cities basic CRUD

---
+++ Ecommerce/Models/Department.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Ecommerce.Models
+{
+    public class Department
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+
+        [Required(ErrorMessage ="The field {0} is requerid")]
+        [MaxLength(50, ErrorMessage ="The filed {0} must be maximum {1} characters lenght")]
+        [Display(Name = "Department")]
+        public string Name { get; set; }
+
+        public virtual ICollection<City> Cities { get; set; }
+    }
+}
